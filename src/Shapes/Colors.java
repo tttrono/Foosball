abf032a1e73acf3e1f42c7322be6bfb39163c09f
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-06-01 23:08:23 +0800

    Removed unused colors.

--- src/Shapes/Colors.java
+++ src/Shapes/Colors.java
@@ -21,35 +21,22 @@ import java.awt.Color;
 public class Colors {
 
 	public static final Color INVISIBLE 	= new Color(0, 0, 0, 0);
-	public static final Color SILVER 		= new Color(203, 202, 218);
+	public static final Color SILVER 	= new Color(203, 202, 218);
 	
-	public static final Color PANTONE 		= new Color(183, 133, 98);
-	public static final Color CREAM 		= new Color(249, 225, 187);
+	public static final Color CREAM 	= new Color(249, 225, 187);
 	public static final Color LIGHT_CREAM 	= new Color(250, 235, 192);
-	public static final Color VARNISH 		= new Color(225, 169, 95);
+	public static final Color VARNISH 	= new Color(225, 169, 95);
 
-	public static final Color DARKESTEST_WOOD	= new Color(53, 28, 3);
 	public static final Color DARKEST_WOOD	= new Color(51, 39, 39);
 	public static final Color DARKER_WOOD	= new Color(70, 39, 8);
-	public static final Color DARK_WOOD		= new Color(113, 98, 65);
-	public static final Color WOOD			= new Color(164, 98, 20);
-	public static final Color WOOD_2		= new Color(219, 143, 49);
+	public static final Color DARK_WOOD	= new Color(113, 98, 65);
+	public static final Color WOOD		= new Color(164, 98, 20);
 	public static final Color LIGHT_WOOD	= new Color(216, 184, 137);
-	public static final Color LIGHT_WOOD_2	= new Color(250, 220, 166);
-	public static final Color MOCHA			= new Color(162, 126, 74);
-	public static final Color LIGHT_MOCHA	= new Color(215, 180, 126);
 	
 	public static final Color CHALKBOARD	= new Color(35, 29, 29);
-	public static final Color GREEN			= new Color(47, 148, 80);
+	public static final Color GREEN		= new Color(47, 148, 80);
 	public static final Color FOREST_GREEN	= new Color(33, 113, 18);
 	
-	public static final Color DARK_TEAL		= new Color(39, 53, 71);
+	public static final Color DARK_TEAL	= new Color(39, 53, 71);
 	public static final Color DARKER_TEAL 	= new Color(33, 46, 62);
-	
-	public static final Color RED_DARK 		= new Color(228, 31, 28);
-	public static final Color RED_BLOOD 	= new Color(143, 29, 29); 
-	
-	public static final Color CYAN 			= new Color(124, 232, 244);
-	public static final Color FUSCHIA 		= new Color(223, 40, 92);
-	
 }
